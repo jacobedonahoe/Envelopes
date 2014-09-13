5f3fb371714f918a00073a263d2fbd0616be5e15
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-09-12 19:27:43 -0500

    Envelope system
    budgeting system

---
+++ Envelopes/Envelope.vb
@@ -0,0 +1,32 @@
+﻿Public Class Envelope
+    Private mName
+    Private mBalance
+
+    Public Sub New(name As String, balance As String)
+        Me.mName = name
+        Me.mBalance = System.Convert.ToDecimal(balance)
+    End Sub
+
+    Property name() As String
+        Get
+            Return mName
+        End Get
+        Set(value As String)
+            mName = value
+        End Set
+    End Property
+    Property Balance() As String
+        Get
+            Return mBalance
+        End Get
+        Set(value As String)
+            mBalance = value
+        End Set
+    End Property
+    Public Function deposit(amount As Integer)
+        Return Me.mBalance = Me.mBalance - amount
+    End Function
+    Public Function withdraw(amount As Integer)
+        Return Me.mBalance = Me.mBalance - amount
+    End Function
+End Class
---
+++ Envelopes/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("Envelopes.txt")
+
+        myReader.TextFieldType = FileIO.FieldType.Delimited
+        myReader.SetDelimiters(",")
+
+        Dim currentRow As String()
+        Dim balance As String
+        Dim name As String
+        Dim envelopesArray As New ArrayList
+
+        While Not myReader.EndOfData
+            Try
+                currentRow = myReader.ReadFields()
+                name = currentRow(0)
+                balance = currentRow(1)
+
+                envelopesArray.Add(generate(name, balance))
+
+            Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                MsgBox("Envelope" & ex.Message & "is not valid and will be skipped.")
+            End Try
+        End While
+
+        Dim envelopeCount As Integer = 0
+
+        For Each Envelope As Envelope In envelopesArray
+
+            Dim envelopePanel As New System.Windows.Forms.Panel
+            envelopePanel.Name = "pnl" & Envelope.name
+            envelopePanel.Location = New Point(0, envelopeCount * 70)
+            envelopePanel.Height = 70
+            envelopePanel.Width = 230
+
+            Dim envelopeNameButton As New System.Windows.Forms.Button
+            envelopeNameButton.Name = "btn" & Envelope.name
+            envelopeNameButton.Text = Envelope.name
+            envelopeNameButton.Location = New Point(10, 10)
+            envelopeNameButton.Height = 50
+            envelopeNameButton.Width = 100
+
+            Dim envelopeBalanceButton As New System.Windows.Forms.Button
+            envelopeBalanceButton.Name = "btn" & Envelope.name & "Balance"
+            envelopeBalanceButton.Text = Envelope.Balance
+            envelopeBalanceButton.Location = New Point(115, 10)
+            envelopeBalanceButton.Height = 25
+            envelopeBalanceButton.Width = 100
+
+            Dim depositButton As New System.Windows.Forms.Button
+            depositButton.Text = "$+"
+            depositButton.ForeColor = Color.ForestGreen
+            depositButton.Location = New Point(115, 35)
+            depositButton.Height = 25
+            depositButton.Width = 50
+
+            Dim withdrawlButton As New System.Windows.Forms.Button
+            withdrawlButton.Text = "$-"
+            withdrawlButton.ForeColor = Color.DarkRed
+            withdrawlButton.Location = New Point(165, 35)
+            withdrawlButton.Height = 25
+            withdrawlButton.Width = 50
+
+            envelopePanel.Controls.Add(envelopeNameButton)
+            envelopePanel.Controls.Add(envelopeBalanceButton)
+            envelopePanel.Controls.Add(depositButton)
+            envelopePanel.Controls.Add(withdrawlButton)
+
+            Me.Controls.Add(envelopePanel)
+            envelopeCount += 1
+        Next
+
+    End Sub
+
+
+
+    Public Function generate(ByRef name, ByRef balance) As Envelope
+        Dim newEnvelope As New Envelope(name, balance)
+        Return newEnvelope
+    End Function
+End Class
