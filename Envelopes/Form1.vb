24416b53dfe9426e5c409455a91ff1241559527e
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-09-19 19:10:52 -0500

    working version

--- Envelopes/Form1.vb
+++ Envelopes/Form1.vb
@@ -1,6 +1,14 @@
 ﻿Imports System.IO
 
 Public Class Form1
+
+    Dim envelopesArray As New ArrayList
+    Dim currentEnvelope As New Object
+    Dim amount As Decimal
+    Dim balanceOfNewEnvelope
+    Dim message As String
+    Dim nameOfNewEnvelope As String
+    Dim blankException As New Exception
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim myReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("Envelopes.txt")
@@ -11,7 +19,6 @@ Public Class Form1
         Dim currentRow As String()
         Dim balance As String
         Dim name As String
-        Dim envelopesArray As New ArrayList
 
         While Not myReader.EndOfData
             Try
@@ -26,18 +33,33 @@ Public Class Form1
             End Try
         End While
 
+        generatePanels()
+    End Sub
+    Public Sub updateEnvelopes()
+        Dim myWriter As New System.IO.StreamWriter("Envelopes.txt", False)
+        For Each Envelope In envelopesArray
+            myWriter.WriteLine(Envelope.name & "," & Envelope.balance & vbCrLf)
+        Next
+        myWriter.Close()
+    End Sub
+    Public Sub refreshForm()
+        updateEnvelopes()
+        Me.Controls.Clear()
+        generatePanels()
+    End Sub
+    Public Sub generatePanels()
         Dim envelopeCount As Integer = 0
 
         For Each Envelope As Envelope In envelopesArray
 
             Dim envelopePanel As New System.Windows.Forms.Panel
             envelopePanel.Name = "pnl" & Envelope.name
-            If envelopeCount < 5 Then
+            If envelopeCount < 7 Then
                 envelopePanel.Location = New Point(0, envelopeCount * 70)
-            ElseIf envelopeCount < 10 Then
-                envelopePanel.Location = New Point(230, envelopeCount * 70)
+            ElseIf envelopeCount < 14 Then
+                envelopePanel.Location = New Point(230, (envelopeCount - 7) * 70)
             Else
-                envelopePanel.Location = New Point(460, envelopeCount * 70)
+                envelopePanel.Location = New Point(460, (envelopeCount - 14) * 70)
             End If
             envelopePanel.Height = 70
             envelopePanel.Width = 230
@@ -58,6 +80,8 @@ Public Class Form1
             envelopeBalanceButton.Width = 100
 
             Dim depositButton As New System.Windows.Forms.Button
+            AddHandler depositButton.Click, AddressOf depositButton_Click
+            depositButton.Name = "btnDeposit" & Envelope.name
             depositButton.Text = "$+"
             depositButton.ForeColor = Color.ForestGreen
             depositButton.Location = New Point(115, 35)
@@ -65,6 +89,8 @@ Public Class Form1
             depositButton.Width = 50
 
             Dim withdrawlButton As New System.Windows.Forms.Button
+            AddHandler withdrawlButton.Click, AddressOf withdrawlButton_Click
+            withdrawlButton.Name = "btnWithdrawl" & Envelope.name
             withdrawlButton.Text = "$-"
             withdrawlButton.ForeColor = Color.DarkRed
             withdrawlButton.Location = New Point(165, 35)
@@ -79,9 +105,59 @@ Public Class Form1
             Me.Controls.Add(envelopePanel)
             envelopeCount += 1
         Next
+        Dim btnAddEnvelope As New System.Windows.Forms.Button
+        AddHandler btnAddEnvelope.Click, AddressOf btnAddEnvelope_Click
+        btnAddEnvelope.Location = New Point(200, 500)
+        btnAddEnvelope.Width = 200
+        btnAddEnvelope.Height = 50
+        btnAddEnvelope.Text = "Add new envelope"
 
+        Me.Controls.Add(btnAddEnvelope)
     End Sub
-    Protected Sub envelopeNameButton_Click(sender As Object, e As EventArgs)
+    Private Sub textChangedEventHandler(ByVal sender As Object, ByVal args As EventArgs)
+        If sender.name = "txtAmount" Then
+            Try
+                amount = Convert.ToDecimal(sender.text)
+            Catch ex As Exception
+                MessageBox.Show("please enter a number")
+            End Try
+        End If
+        If sender.name = "txtMessage" Then
+            message = sender.text
+        End If
+        If sender.name = "txtNameOfEnvelope" Then
+            Try
+                If sender.text = "" Then
+                    Throw blankException
+                End If
+                balanceOfNewEnvelope = (sender.text)
+            Catch ex As Exception
+                MessageBox.Show("cannot be left blank")
+            End Try
+            nameOfNewEnvelope = sender.text
+        End If
+        If sender.name = "txtBalanceOfEnvelope" Then
+            Try
+                If sender.text = "" Then
+                    Throw blankException
+                End If
+                balanceOfNewEnvelope = (sender.text)
+            Catch ex As Exception
+                MessageBox.Show("cannot be left blank")
+            End Try
+        End If
+    End Sub
+    Private Sub btnDepositSubmit_Click(sender As Object, e As EventArgs)
+        currentEnvelope.deposit(amount, message)
+        refreshForm()
+        sender.parent.close()
+    End Sub
+    Private Sub btnWithdrawlSubmit_Click(sender As Object, e As EventArgs)
+        currentEnvelope.withdraw(amount, message)
+        refreshForm()
+        sender.parent.close()
+    End Sub
+    Private Sub envelopeNameButton_Click(sender As Object, e As EventArgs)
         Dim frmLog As New System.Windows.Forms.Form
         frmLog.Height = 600
         frmLog.Width = 600
@@ -110,7 +186,199 @@ Public Class Form1
     Private Sub btnExit_Click(sender As Object, e As EventArgs)
         sender.parent.Close()
     End Sub
+    Private Sub depositButton_Click(sender As Object, e As EventArgs)
+        currentEnvelope = findEnvelopeByName(sender.name.remove(0, 10))
+        Dim frmDeposit As New System.Windows.Forms.Form
+        frmDeposit.Height = 600
+        frmDeposit.Width = 600
 
+        Dim lblAmount As New System.Windows.Forms.Label
+        lblAmount.Location = New Point(10, 10)
+        lblAmount.Text = "Amount"
+        lblAmount.Height = 15
+        lblAmount.Width = 100
+
+        Dim txtAmount As New System.Windows.Forms.TextBox
+        AddHandler txtAmount.TextChanged, AddressOf textChangedEventHandler
+        txtAmount.Name = "txtAmount"
+        txtAmount.Location = New Point(10, 30)
+        txtAmount.Width = 100
+
+        Dim lblMessage As New System.Windows.Forms.Label
+        lblMessage.Location = New Point(120, 10)
+        lblMessage.Text = "Message"
+        lblMessage.Height = 15
+        lblMessage.Width = 100
+
+        Dim txtMessage As New System.Windows.Forms.TextBox
+        AddHandler txtMessage.TextChanged, AddressOf textChangedEventHandler
+        txtMessage.Name = "txtMessage"
+        txtMessage.Height = 50
+        txtMessage.Width = 100
+        txtMessage.Location = New Point(120, 30)
+        txtMessage.ScrollBars = ScrollBars.Both
+        txtMessage.MaxLength = 100
+        txtMessage.Multiline = True
+
+        Dim btnSubmit As New System.Windows.Forms.Button
+        AddHandler btnSubmit.Click, AddressOf btnDepositSubmit_Click
+        btnSubmit.Height = 50
+        btnSubmit.Width = 100
+        btnSubmit.Location = New Point(10, 100)
+        btnSubmit.Text = "Deposit"
+
+        Dim btnReturn As New System.Windows.Forms.Button
+        AddHandler btnReturn.Click, AddressOf btnExit_Click
+        btnReturn.Height = 50
+        btnReturn.Width = 100
+        btnReturn.Text = "Return"
+        btnReturn.Location = New Point(120, 100)
+
+
+        frmDeposit.Controls.Add(lblAmount)
+        frmDeposit.Controls.Add(lblMessage)
+        frmDeposit.Controls.Add(txtAmount)
+        frmDeposit.Controls.Add(txtMessage)
+        frmDeposit.Controls.Add(btnSubmit)
+        frmDeposit.Controls.Add(btnReturn)
+
+        frmDeposit.Show()
+    End Sub
+    Private Sub withdrawlButton_Click(sender As Object, e As EventArgs)
+        currentEnvelope = findEnvelopeByName(sender.name.remove(0, 12))
+        Dim frmWithdrawl As New System.Windows.Forms.Form
+        frmWithdrawl.Height = 600
+        frmWithdrawl.Width = 600
+
+        Dim lblAmount As New System.Windows.Forms.Label
+        lblAmount.Location = New Point(10, 10)
+        lblAmount.Text = "Amount"
+        lblAmount.Height = 15
+        lblAmount.Width = 100
+
+        Dim txtAmount As New System.Windows.Forms.TextBox
+        AddHandler txtAmount.TextChanged, AddressOf textChangedEventHandler
+        txtAmount.Name = "txtAmount"
+        txtAmount.Location = New Point(10, 30)
+        txtAmount.Width = 100
+
+        Dim lblMessage As New System.Windows.Forms.Label
+        lblMessage.Location = New Point(120, 10)
+        lblMessage.Text = "Message"
+        lblMessage.Height = 15
+        lblMessage.Width = 100
+
+        Dim txtMessage As New System.Windows.Forms.TextBox
+        AddHandler txtMessage.TextChanged, AddressOf textChangedEventHandler
+        txtMessage.Name = "txtMessage"
+        txtMessage.Height = 50
+        txtMessage.Width = 100
+        txtMessage.Location = New Point(120, 30)
+        txtMessage.ScrollBars = ScrollBars.Both
+        txtMessage.MaxLength = 100
+        txtMessage.Multiline = True
+
+        Dim btnSubmit As New System.Windows.Forms.Button
+        AddHandler btnSubmit.Click, AddressOf btnWithdrawlSubmit_Click
+        btnSubmit.Height = 50
+        btnSubmit.Width = 100
+        btnSubmit.Location = New Point(10, 100)
+        btnSubmit.Text = "Withdraw"
+
+        Dim btnReturn As New System.Windows.Forms.Button
+        AddHandler btnReturn.Click, AddressOf btnExit_Click
+        btnReturn.Height = 50
+        btnReturn.Width = 100
+        btnReturn.Text = "Return"
+        btnReturn.Location = New Point(120, 100)
+
+
+        frmWithdrawl.Controls.Add(lblAmount)
+        frmWithdrawl.Controls.Add(lblMessage)
+        frmWithdrawl.Controls.Add(txtAmount)
+        frmWithdrawl.Controls.Add(txtMessage)
+        frmWithdrawl.Controls.Add(btnSubmit)
+        frmWithdrawl.Controls.Add(btnReturn)
+
+        frmWithdrawl.Show()
+    End Sub
+    Private Sub btnAddEnvelope_Click()
+        Dim frmAddEnvelope As New System.Windows.Forms.Form
+
+        Dim lblName As New System.Windows.Forms.Label
+        lblName.Height = 20
+        lblName.Location = New Point(10, 10)
+        lblName.Text = "Name of Envelope"
+        lblName.Width = 100
+
+        Dim txtNameOfEnvelope As New System.Windows.Forms.TextBox
+        txtNameOfEnvelope.Text = "name"
+        AddHandler txtNameOfEnvelope.TextChanged, AddressOf textChangedEventHandler
+        txtNameOfEnvelope.Height = 20
+        txtNameOfEnvelope.Location = New Point(10, 40)
+        txtNameOfEnvelope.Name = "txtNameOfEnvelope"
+        txtNameOfEnvelope.Width = 100
+
+        Dim lblBalance As New System.Windows.Forms.Label
+        lblBalance.Height = 20
+        lblBalance.Location = New Point(120, 10)
+        lblBalance.Text = "Starting Balance of Envelope"
+        lblBalance.Width = 100
+
+        Dim txtBalanceOfEnvelope As New System.Windows.Forms.TextBox
+        txtBalanceOfEnvelope.Text = "0"
+        AddHandler txtBalanceOfEnvelope.TextChanged, AddressOf textChangedEventHandler
+        txtBalanceOfEnvelope.Height = 20
+        txtBalanceOfEnvelope.Location = New Point(120, 40)
+        txtBalanceOfEnvelope.Name = "txtBalanceOfEnvelope"
+        txtBalanceOfEnvelope.Width = 100
+
+        Dim btnAdd As New System.Windows.Forms.Button
+        AddHandler btnAdd.Click, AddressOf btnAdd_Click
+        btnAdd.Height = 50
+        btnAdd.Location = New Point(10, 70)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Text = "Add Envelope"
+        btnAdd.Width = 100
+
+        Dim btnExit As New System.Windows.Forms.Button
+        AddHandler btnExit.Click, AddressOf btnExit_Click
+        btnExit.Height = 50
+        btnExit.Location = New Point(120, 70)
+        btnExit.Name = "btnExit"
+        btnExit.Text = "Return"
+        btnExit.Width = 100
+
+        frmAddEnvelope.Controls.Add(lblBalance)
+        frmAddEnvelope.Controls.Add(txtNameOfEnvelope)
+        frmAddEnvelope.Controls.Add(txtBalanceOfEnvelope)
+        frmAddEnvelope.Controls.Add(lblName)
+        frmAddEnvelope.Controls.Add(btnAdd)
+        frmAddEnvelope.Controls.Add(btnExit)
+        frmAddEnvelope.Show()
+    End Sub
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs)
+        Dim newEnvelope As Envelope
+        If Not (nameOfNewEnvelope) = ("") Or Not (balanceOfNewEnvelope) = ("") Then
+            newEnvelope = generate(nameOfNewEnvelope, balanceOfNewEnvelope)
+            envelopesArray.Add(newEnvelope)
+            newEnvelope.create()
+            updateEnvelopes()
+            refreshForm()
+            sender.parent.close()
+        Else
+            MessageBox.Show("Both inputs must be filled!")
+        End If
+    End Sub
+    Public Function findEnvelopeByName(name)
+        Dim envelopeWeWant As New Object
+        For Each Envelope In envelopesArray
+            If Envelope.name = name Then
+                envelopeWeWant = Envelope
+            End If
+        Next
+        Return envelopeWeWant
+    End Function
     Public Function generate(ByRef name, ByRef balance) As Envelope
         Dim newEnvelope As New Envelope(name, balance)
         Return newEnvelope
